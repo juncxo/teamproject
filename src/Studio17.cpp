288052ad4dc17e53eb00ffae2dc8f8514edf3f91
arannat <a.tasnim@wustl.edu>
2023-04-13 10:35:45 -0500

    studio 17 IP

--- src/Studio17.cpp
+++ src/Studio17.cpp
@@ -4,5 +4,10 @@
 using namespace std;
 
 int main (int argc, char * argv[]) {
-
+    ImageFile* tf = new ImageFile("hello");
+    vector <char> vc = {'a', 'b', 'c', 'd', 'e'};
+    tf->write(vc);
+    tf->read();
+    cout << tf->getName()<<endl;
+    cout << tf->getSize()<<endl;
 }
