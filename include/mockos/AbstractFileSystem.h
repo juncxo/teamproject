899ca30cfe3ba2527a96e31b5ef2ff505a7e5290
arannat <a.tasnim@wustl.edu>
2023-04-15 13:08:11 -0500

    studio 17 (3)

---
+++ include/mockos/AbstractFileSystem.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#include "AbstractFile.h"
+
+class AbstractFileSystem : public AbstractFile{
+public:
+    virtual int addFile(std::string, AbstractFile*) = 0;
+    virtual int createFile(std::string) = 0;
+    virtual int deleteFile(std::string) = 0;
+    virtual AbstractFile* openFile(std::string) = 0;
+    virtual int closeFile(AbstractFile*) = 0;
+};
---
+++ include/mockos/SimpleFileSystem.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "AbstractFileSystem.h"
+
+class SimpleFileSystem : public AbstractFileSystem{
+
+};
