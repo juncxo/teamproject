c45183beec42e2b902ec1508c763ae4af783388c
arannat <a.tasnim@wustl.edu>
2023-05-05 13:06:03 -0500

    DONE DONE DONE

--- lib/mockos/PasswordProxy.cpp
+++ lib/mockos/PasswordProxy.cpp
@@ -5,20 +5,26 @@
 using namespace std;
 
 PasswordProxy::PasswordProxy(AbstractFile* af, std::string str) {
-    password = str;
 
+    password = str;
     vector <int> encryptedPassword;
+
+    //encrypt each character's ASCII value
     for (int i = 0; i < password.length(); i++) {
         int ASCIIValue = (int) password[i];
         int store = encryptMod131 (ASCIIValue);
         encryptedPassword.push_back(store);
     }
     password = "";
+
+    //the password is now encrypted to be the concatenation of the encrypted
+    //versions of its ASCII values. For instance, the password "hello15b"
+    //gets encrypted to "89 93 114 114 126 40 116 28"
+
     for (int i = 0; i < encryptedPassword.size(); i++) {
         password += (std::to_string(encryptedPassword[i]) + " ");
 
     }
-    //cout << "Encrypted password: " << password << endl;
 
     cout << endl;
 
@@ -30,6 +36,8 @@ PasswordProxy::~PasswordProxy() {
     delete protectedFile;
 }
 
+//prompt for a password using getline. This function is called whenever a user tries
+//to edit or display a password-protected file.
 string PasswordProxy::passwordPrompt() {
     string input;
     cout << "Enter a password" << endl;
@@ -38,11 +46,14 @@ string PasswordProxy::passwordPrompt() {
 
 }
 
+//helper function to encrypt each character (based on ASCII value)
 int PasswordProxy::encryptMod131 (int decryptedNumber) {
     return twoToThe(decryptedNumber);
 }
 
+//helper function to decrypt each character (based on ASCII value)
 int PasswordProxy::decryptMod131 (int encryptedNumber) {
+    //to decrypt each character, we calculate the discrete logarithm for each encrypted ASCII value
     for (int i = 0; i < 131; i++) {
         if (twoToThe(i) == encryptedNumber) {
             return i;
@@ -50,6 +61,7 @@ int PasswordProxy::decryptMod131 (int encryptedNumber) {
     }
 }
 
+//helper function to compute the value of 2^exponent mod 131 (used for encryption and descryption)
 int PasswordProxy::twoToThe (int exponent) {
     int mod131 = 1;
     for (int i = 0; i < exponent; i++) {
@@ -59,17 +71,24 @@ int PasswordProxy::twoToThe (int exponent) {
     return mod131;
 }
 
+//function to check if an entered password is correct (called when the user
+//tries to display or edit a password-protected file)
 bool PasswordProxy::passwordCheck(string str){
+    //we decode the password here.
     vector<int> decodedCharacters;
     string decodedPassword;
     int numberOfSpaces = 0;
     int index = 0;
+
+    //count the number of ASCII values in the encrypted password
     for (int i = 0; i < password.length(); i++) {
         if (password[i] == ' ') {
             numberOfSpaces++;
         }
     }
     istringstream iss (password);
+
+    //extract each ASCII value from the password and decrypt it
     while (index < numberOfSpaces){
         int ASCIIValue;
         iss >> ASCIIValue;
@@ -78,10 +97,12 @@ bool PasswordProxy::passwordCheck(string str){
         index++;
     }
 
-    //convert vector<int> to string
+    //convert the vector<int> of decoded ASCII values to a decoded password (string)
     for (int i = 0; i < decodedCharacters.size(); i++) {
         decodedPassword += (char) (decodedCharacters[i]);
     }
+
+    //check if the entered password equals the original (decoded) password
     if(str == decodedPassword){
         return true;
     }
@@ -89,7 +110,7 @@ bool PasswordProxy::passwordCheck(string str){
 }
 
 
-
+//returns the contents of the file upon entering correct password
 vector<char> PasswordProxy::read() {
     vector<char> pv;
     string inputPass = passwordPrompt();
@@ -99,6 +120,7 @@ vector<char> PasswordProxy::read() {
     return pv;
 }
 
+//writes to the file upon entering correct password
 int PasswordProxy::write(vector<char> pv){
     string inputPass = passwordPrompt();
     if(passwordCheck(inputPass)){
@@ -106,6 +128,8 @@ int PasswordProxy::write(vector<char> pv){
     }
     return incorrectPassword;
 }
+
+//appends to the file upon entering correct password
 
 int PasswordProxy::append(vector<char> pv){
     string inputPass = passwordPrompt();
@@ -115,10 +139,12 @@ int PasswordProxy::append(vector<char> pv){
     return passwordUnableToAppend;
 }
 
+//gets the size of the file
 unsigned int PasswordProxy::getSize(){
     return protectedFile->getSize();
 }
 
+//gets the name of the file
 string PasswordProxy::getName(){
     return protectedFile->getName();
 }
